95e2fe3b30600647dc776cbc4df1c8a8acada3a6
Tom Hudson <tom.hudson89@gmail.com>
2020-02-01 16:11:16 +0000

    done the routes and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_152856) do
+ActiveRecord::Schema.define(version: 2020_02_01_153816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2020_02_01_152856) do
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "booking_id"
+    t.text "content"
+    t.string "Title"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,4 +66,5 @@ ActiveRecord::Schema.define(version: 2020_02_01_152856) do
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
+  add_foreign_key "reviews", "bookings"
 end
