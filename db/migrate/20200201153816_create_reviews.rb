9c9b5a462da2feb6032b842527e3b0b76221b7e4
Stephen Pinshon <spinshon@hotmail.com>
2020-02-01 15:39:02 +0000

    Added Review Model

---
+++ db/migrate/20200201153816_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.references :booking, foreign_key: true
+      t.text :content
+      t.string :Title
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_151755) do
+ActiveRecord::Schema.define(version: 2020_02_01_152856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "flat_id"
+    t.string "start"
+    t.date "date"
+    t.string "end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.bigint "user_id"
@@ -41,5 +53,7 @@ ActiveRecord::Schema.define(version: 2020_02_01_151755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
 end
