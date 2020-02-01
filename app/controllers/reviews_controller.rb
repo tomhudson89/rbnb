c0ff82da872cffc37882f1d137bc006b3378139c
Stephen Pinshon <spinshon@hotmail.com>
2020-02-01 16:45:17 +0000

    Added Reviews Controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,42 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to reviews_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to reviews_path(@review)
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating)
+  end
+end
