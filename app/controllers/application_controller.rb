f04865bdbb1f8116335c5857514cbf383484ce8c
Guillermo Forteza <guifors@gmail.com>
2020-02-11 19:48:48 +0000

    added show page for host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
