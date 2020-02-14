7b4efdd131ac828330069a480ccc66ff86110446
Tom Hudson <tom.hudson89@gmail.com>
2020-02-14 11:34:55 +0000

    updates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,14 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
   has_many :reviews
+
+  def total_days
+    (end_date - start_date).to_i
+  end
+
+  def stay_cost
+    days = total_days
+    (days * self.flat.price_per_night)
+  end
 end
 
